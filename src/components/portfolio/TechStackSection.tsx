5fa11b519ea5ce216c984b8c94504016dbf9a57e
Anshuman Dhami <dhamianshuman@gmail.com>
2025-07-11 00:57:33 +0000

    okay i want you to remove the svg icons for the programming languages i

--- src/components/portfolio/TechStackSection.tsx
+++ src/components/portfolio/TechStackSection.tsx
@@ -11,10 +11,12 @@ export default function TechStackSection() {
             <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4">
               {category.skills.map((tech) => (
                 <div key={tech.name} className="flex flex-col items-center justify-center p-4 rounded-lg bg-secondary/30 transition-all hover:bg-secondary/70 hover:shadow-lg">
-                  <div
-                    className="h-10 w-10 mb-2 [&>svg]:w-full [&>svg]:h-full"
-                    dangerouslySetInnerHTML={{ __html: tech.icon }}
-                  />
+                  {tech.icon && (
+                    <div
+                      className="h-10 w-10 mb-2 [&>svg]:w-full [&>svg]:h-full"
+                      dangerouslySetInnerHTML={{ __html: tech.icon }}
+                    />
+                  )}
                   <p className="text-sm font-medium text-center">{tech.name}</p>
                 </div>
               ))}
