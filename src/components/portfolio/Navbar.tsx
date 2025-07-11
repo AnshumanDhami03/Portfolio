d1b43a421ec20de7601ba34dcf4d13f7c35ab8a9
Anshuman Dhami <dhamianshuman@gmail.com>
2025-07-11 10:18:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Navbar activeProf

--- src/components/portfolio/Navbar.tsx
+++ src/components/portfolio/Navbar.tsx
@@ -51,7 +51,7 @@ export default function Navbar({ activeProfile, onProfileToggle }: NavbarProps) 
   };
 
   return (
-    <nav className="bg-background">
+    <nav className="sticky top-0 z-50 bg-background/80 backdrop-blur-sm">
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6">
         <div className="flex items-center justify-between h-16">
           <div className="flex flex-col">
